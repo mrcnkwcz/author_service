b98e74911a01e8efdbe5918dc9cdde378e7cc3ca
Artur Martsinkevich <mrcnkwcz@yandex.com>
2023-12-17 14:47:53 +0300

    Serverless Fix

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,17 @@
-FROM python:3.8-slim
+# 
+FROM python:3.10-slim
 
-WORKDIR /app
+# 
+WORKDIR /code
 
-ENV LANG C.UTF-8
-ENV LC_ALL C.UTF-8
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
+# 
+COPY ./requirements.txt /code/requirements.txt
 
-RUN apt-get update \
-    && apt-get install gcc -y \
-    && apt-get install pipenv -y \
-    && apt-get clean
+# 
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
-COPY ./Pipfile .
-RUN PIPENV_VENV_IN_PROJECT=1 pipenv install --skip-lock --system --dev
+# 
+COPY ./app /code/app
 
-COPY . /app/
+# 
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
--- app/main.py
+++ app/main.py
@@ -1,31 +1,21 @@
 from fastapi import FastAPI
 
-from .api import authors, models
-from .api.database import database as db, engine
+app = FastAPI()
 
-models.metadata.create_all(engine)
+authors = [
+    {'id': 1, 'name':'Alice', 'nationality': 'A'},
+    {'id': 2, 'name':'Bob', 'nationality': 'B'},
+    {'id': 3, 'name':'Eva', 'nationality': 'C'},
+]
 
-app = FastAPI(
-    title="Author API",
-    openapi_url="/api/v1/authors/openapi.json",
-    docs_url="/api/v1/authors/docs"
-)
-
-
-@app.on_event("startup")
-async def startup():
-    await db.connect()
+@app.get("/")
+def all_authors():
+    return authors
 
 
-@app.on_event("shutdown")
-async def shutdown():
-    await db.disconnect()
-
-
-app.include_router(authors.authors, prefix="/api/v1/authors", tags=["authors"])
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="localhost", port=8001, log_level="debug", reload=True)
+@app.get("/{author_id}")
+def read_item(author_id: int):
+    try:
+        return authors[author_id]
+    except IndexError:
+        return "Author not found"
